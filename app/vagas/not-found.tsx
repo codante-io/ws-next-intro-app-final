8c1719b5dbf50f66c9896b1c7e9434c545a2500c
Roberto Cestari <robertotcestari@gmail.com>
2025-01-24 10:49:43 -0300

    add shadcn

--- app/vagas/not-found.tsx
+++
@@ -1,9 +0,0 @@
-export default function NotFound() {
-  return (
-    <div>
-      <h2>ATENção nada Encontrado</h2>
-      <p>Não foi possível encontrar o recurso solicitado</p>
-      <img src="/construction.png" alt="ilustração de uma construção" />
-    </div>
-  );
-}
