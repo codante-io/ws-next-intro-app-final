cd508db7f2c479fc6a0ddda4e734ff6283287350
Roberto Cestari <robertotcestari@gmail.com>
2025-01-24 09:41:23 -0300

    adding routing

---
+++ app/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Não Encontrado</h2>
+      <p>Não foi possível encontrar o recurso solicitado</p>
+      <img src="/boat.png" alt="ilustração de um barco" />
+    </div>
+  );
+}
---
+++ app/vagas/not-found.tsx
@@ -0,0 +1,9 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h2>ATENção nada Encontrado</h2>
+      <p>Não foi possível encontrar o recurso solicitado</p>
+      <img src="/construction.png" alt="ilustração de uma construção" />
+    </div>
+  );
+}
