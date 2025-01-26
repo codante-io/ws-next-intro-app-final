e2d0c2e6445121d615ba4b1fb8d56e46e7799b88
Roberto Cestari <robertotcestari@gmail.com>
2025-01-26 10:00:30 -0300

    next font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Navbar from "@/components/layout/navbar";
 import Footer from "@/components/layout/footer";
+import { Maven_Pro } from "next/font/google";
 
 export const metadata: Metadata = {
   title: "CodanteVagas",
@@ -9,13 +10,20 @@ export const metadata: Metadata = {
     "O CodanteVagas conecta candidatos a empregos ideais, oferecendo funcionalidades intuitivas para busca e gerenciamento de vagas.",
 };
 
+const mavenPro = Maven_Pro({
+  subsets: ["latin"],
+  variable: "--font-maven-pro",
+});
+
+console.log();
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={mavenPro.variable}>
       <body>
         <div className="flex min-h-screen flex-col">
           <Navbar />
